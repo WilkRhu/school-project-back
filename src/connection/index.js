14d65fd0232b753135c9d1867f8a50295606ffdb
Wilk Franca <wilk.caetano@gmail.com>
2021-05-24 21:06:58 -0300

    Audit fix and process env connection

--- src/connection/index.js
+++ src/connection/index.js
@@ -1,13 +1,13 @@
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'school'
+  user     : process.env.DATAUSER,
+  password : process.env.DATAPASSWORD,
+  database : process.env.DATABASE
 });
  
 connection.connect(() => {
     console.log("Conectado ao banco de dados!")
 });
  
-//connection.end();
+connection.end();
